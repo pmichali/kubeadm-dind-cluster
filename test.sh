d0331c9e9e1a394565ac1ba18b51f67cd85d51d6
Paul Michali <pc@michali.net>
2017-08-18 06:17:32 +0000

    Add IPv6 only support to DinD
    This commit adds support for IPv6 only mode for a Kubernetes cluster used with DinD.
    To do this, it uses bind9 for DNS32, and Tayga for NAT64. Intention here, is so that
    this can be used for E2E testing, where we have an IPv6 only cluster, but is connected
    to an IPv4 only network (e.g. running on Google Compute Engine VM).
    
    Notes:
    - Uses /80 pod network that is contained within DinD subnet (like done for v4).
    - Several environment variables can be used to customize.
    - Uses new CNI 0.6.0 plugin.
    - Uses Tagya container (built by Daneyon Hansen) for NAT64 (include into DinD build?).
    - Forces all external DNS lookups to return A record, and will build an IPv4 embedded
      IPv6 address to refer to the node.
    - Dual stack could be added in future.
    - NAT66 container could be used for pure IPv6 or dual stack environment (along with
      DNS64 config change).
    - NAT64 requires static route on host, which is added (using sudo) by script,
      if missing. Can be set up in advance, if desired.
    - Allows DNS server to be specified for forwarded requests, in case in an environment
      where host doesn't allow forwarding to DNS servers outside of corporate network.
    - Since not released, one needs to build and run with local DinD image.
    - README suggests in-flight IPv6 patches and work-arounds for proper IPv6 operation.
    - Includes fix to issue where failure to stop kube-dns or proxy pods does not restore
      the controller manifest.
    - IPv4 mode should work the same as before, only using new CNI plugin.
    - Added src-working-area test case to test.sh to allow testing of DinD with local
     Kubernetes repo.
    - Hairpin mode enabled for CNI plugin (precludes IPv6 DAD, until kernel is updated).
    - Temp change to Kubernetes to prevent conntrack setting changes, based on bug 50787.
    
    Questions:
    - Is there a way to create static route on host w/o sudo for NAT64 server?
    
    Issues:
    - On some systems, seeing CNI config file and manifests disappearing, preventing
      kubelet from running.
    - Sometimes see etcdserver timeouts during cluster bring up (not on all systems).
    - Once kube-proxy is modified to allow conntrack settings override, DinD will need
      to be abled to modify the kube-proxy config file.
    
    TODOs:
    - Verify on GCE using gce-setup.sh (having issues as no released image to use).
    - Look at CI failures (working with changeset + kubernetes w/IPv6 changes, not
      tested for older images).
    - Pull CNI plugin install, once CNI 0.6.0 plugin is part of Kubernetes.
    
    FUTURES:
    - Dual stack.
    - IPv6 only (externally as well). Currently, github.com doesn't support IPv6.
    - Pod subnet exclusive of DinD subnet.

--- test.sh
+++ test.sh
@@ -254,6 +254,10 @@ function test-case-src-master-weave {
   )
 }
 
+function test-case-src-working-area {
+  test-cluster-src
+}
+
 if [[ ! ${TEST_CASE} ]]; then
   test-case-1.5
   test-case-1.5-flannel
